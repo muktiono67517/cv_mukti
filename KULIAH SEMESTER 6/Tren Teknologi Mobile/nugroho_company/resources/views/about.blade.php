7fc3673712ee1ff7aa1fed0063e5055f5969751c
Muktiono Nugroho <jujurbaik7@gmail.com>
2024-03-20 23:53:38 +0800

    initial commit

---
+++ KULIAH SEMESTER 6/Tren Teknologi Mobile/nugroho_company/resources/views/about.blade.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>Nugroho Company</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+     <!-- bootstrap css -->
+         <link rel="stylesheet" type="text/css" href="{{ asset('/css/bootstrap.min.css') }}">
+      <!-- style css -->
+         <link rel="stylesheet" type="text/css" href="{{ asset('/css/style.css') }}">
+<!-- Responsive-->
+         <link rel="stylesheet" href="{{ asset('/css/responsive.css') }}">
+<!-- fevicon -->
+         <link rel="icon" href="{{ asset('/images/fevicon.png') }}" type="image/gif" />
+<!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="{{ asset('/css/jquery.mCustomScrollbar.min.css') }}">
+<!-- Tweaks for older IEs-->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+<!-- fonts -->
+      <link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
+<!-- font awesome -->
+      <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<!-- owl stylesheets -->
+      <link href="https://fonts.googleapis.com/css?family=Great+Vibes|Poppins:400,700&display=swap&subset=latin-ext" rel="stylesheet">
+      <link rel="stylesheet" href="{{ asset('/css/owl.carousel.min.css') }}">
+      <link rel="stylesheet" href="{{ asset('/css/owl.theme.default.min.css') }}">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+
+   </head>
+   <body>
+      <!-- banner bg main start -->
+      <div class="banner_bg_main">
+         <!-- header top section start -->
+         <div class="container">
+            <div class="header_section_top">
+               <div class="row">
+                  <div class="col-sm-12">
+                     <div class="custom_menu">
+                        
+                        <ul>
+                        <li><a href="#">Rilis Terbaru</a></li>
+                           <li><a href="#">Penawaran Hari Ini</a></li>
+                           <li><a href="#">Pelayanan Pelanggan</a></li>
+                           
+
+ <link rel="stylesheet" href="desain-khusus.css">
+ 
+
+
+         
+</head>
+<html lang="en">
+<head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <form action="" method="POST" class="login-email">
+</form>
+          
+                        </ul>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <!-- header top section start -->
+         <!-- logo section start -->
+         <div class="logo_section">
+            <div class="container">
+               <div class="row">
+                  <div class="col-sm-12">
+                  </div>
+               </div>
+            </div>
+         </div>
+         <!-- logo section end -->
+         <!-- header section start -->
+         <div class="header_section">
+            <div class="container">
+               <div class="containt_main">
+                  <div id="mySidenav" class="sidenav">
+                     <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                     <a href="#">Dashboard</a>
+                     <a href="#">T-Shirt</a>
+                     <a href="#">Jacket & Hoodie</a>
+                     <a href="#">Pants</a>
+                  </div>
+                  <span class="toggle_icon" onclick="openNav()"><img src="images/toggle-icon.png"></span>
+                  <div class="dropdown">
+                     <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Semua Kategori
+                     </button>
+                     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="#">Pria </a>
+                        <a class="dropdown-item" href="#">Wanita</a>
+                     </div>
+                  </div>
+                  <div class="main">
+                     <!-- Another variation with a button -->
+                     <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Cari di sini">
+                        <div class="input-group-append">
+                           <button class="btn btn-secondary" type="button" style="background-color: #998F99; border-color:#998F99 ">
+                           <i class="fa fa-search"></i>
+                           </button>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="header_box">
+                     <div class="lang_box ">
+                        <a href="#" title="Bahasa" class="nav-link" data-toggle="dropdown" aria-expanded="true">
+                        <img src="images/indonesia-flag.png" class="mr-2" alt="flag">
+                        <alt="flag" class="mr-2 " title="Indonesia"> Indonesia <i class="fa fa-angle-down ml-2" aria-hidden="true"></i>
+                        
+                        </a>
+      
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <!-- header section end -->
+        <!-- banner section start -->
+        <div class="banner_section layout_padding">
+            <div class="container">
+               <div id="my_slider" class="carousel slide" data-ride="carousel">
+                  <div class="carousel-inner">
+                     <div class="carousel-item active">
+                        <div class="row">
+                           <div class="col-sm-12">
+                              <h1 class="banner_taital">Nugroho Company<br></h1>
+                              <h3  class="banner_taital" style="font-size: 30px;">Berpengalaman di bidang pakaian</h3>
+                              <br><br><br><br><br>
+                           </div>
+                        </div>
+                     </div>
+               </div>
+            </div>
+         </div>
+         <!-- banner section end -->
+      
+         <!-- banner bg main end -->
+     
+         <!-- fashion section start -->
+      <div class="fashion_section">
+         <div id="main_slider" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+               <div class="carousel-item active">
+                  <div class="container"><br><br><br><br><br><br>
+                     <h1 class="fashion_taital">About us</h1>
+                     <div class="fashion_section_2">
+                        <div class="row">
+                           <div class="col-lg-4 col-sm-4">
+                              <div class="box_main">
+                                 <h4 class="shirt_text">Sejarah Perusahaan</h4>
+                                 <div class="tshirt_img">Dibangun atas visi Muktiono Nugroho, Nugroho Company berkembang dari sebuah impian menjadi sebuah merek yang dikenal secara nasional maupun internasional dalam dunia fashion. ejak didirikan pada tahun 2024, kami telah mendapatkan pengakuan atas komitmen kami terhadap kualitas, inovasi, dan keberlanjutan.</div>
+                              </div>
+                           </div>
+                           <div class="col-lg-4 col-sm-4">
+                              <div class="box_main">
+                                 <h4 class="shirt_text">Visi dan Misi</h4>
+                                 <div class="tshirt_img">Visi kami adalah menjadi pemimpin dalam pakaian yang berkelanjutan, menciptakan tren yang mempromosikan gaya hidup yang bertanggung jawab dan ramah lingkungan.<br><br> Misi kami adalah menyediakan produk berkualitas tinggi yang diproduksi secara etis dan ramah lingkungan, serta menginspirasi pelanggan kami untuk mengadopsi kebiasaan berbelanja yang lebih berkelanjutan.</div>
+                                       
+                              </div>
+                           </div>
+                           <div class="col-lg-4 col-sm-4">
+                              <div class="box_main">
+                                 <h4 class="shirt_text">Tim Manajemen</h4>
+                                 <div class="tshirt_img"><ul>
+                                    <li>Pemilik: Muktiono Nugroho</li>
+                                    <li>Anggota Tim:
+                                    <ul>
+                                       <li>1. John</li>
+                                       <li>2. Budi</li>
+                                       <li>3. Smith</li>
+                                       <li>4. Putri</li>
+                                       <li>5. Layla</li>
+                                    </ul>
+                                    </li>
+                                    </ul>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+      <!-- fashion section end -->
+      <script>
+         var welcomeMessage ="Selamat datang di Nugroho Company! Kami menginspirasi melalui desain fashion inovatif, mengekspresikan identitas unik dan memberikan pengalaman berbelanja yang memuaskan.";
+         function displayWelcomeMessage() {
+            document.getElementById("welcome").innerHTML = welcomeMessage;
+        }
+      </script>
+
+
+<body onload="displayWelcomeMessage()">
+   <div style="text-align: center; font-size: 20px;" id="welcome"></div>
+</body>
+      
+      <!-- footer section start -->
+      <br><br>
+      <div class="footer_section layout_padding">
+         <div class="container_for_about_and_contact">
+            <a href="/home"><h4 style="color: white;">Home</h4></a>
+         </div>
+      </div>
+      <div class="footer_section layout_padding">
+         <div class="container_for_about_and_contact">
+            <a href="/contact"><h4 style="color: white;">Contact us</h4></a>
+         </div>
+      </div>
+      <!-- footer section end -->
+      <!-- copyright section start -->
+      <div class="copyright_section">
+         <div class="container"><br><br><br>
+            <p class="copyright_text" >&copy;2024</span> Nugroho Company</a></p>
+         </div>
+      </div>
+      <!-- copyright section end -->
+</body>
+</html>
+
+<style>
+   .container_for_about_and_contact {
+       background-color: #333; /* Digunakan untuk membedakan latar belakang */
+       padding: 20px; /* Memberikan ruang di sekitar teks */
+       text-align: center;
+   }
+</style>
